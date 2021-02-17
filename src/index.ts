d63a806d6b462f0c881e08a89268c7ce2f85108b
Tony Fettes <tonyfettes@tonyfettes.com>
2021-02-17 19:37:58 +0800

    feat: Adopt textEdit to replace insertText bug: cannot work when use TAB

--- src/index.ts
+++ src/index.ts
@@ -31,20 +31,78 @@ export async function activate(context: ExtensionContext): Promise<void> {
 
     // Completion Source
     languages.registerCompletionItemProvider('rime', 'IM', null, {
-      async provideCompletionItems(_document: TextDocument, _position: Position, _token: CancellationToken, context: CompletionContext): Promise<CompletionList | undefined | null> {
+      async provideCompletionItems(document: TextDocument, position: Position, _token: CancellationToken, context: CompletionContext): Promise<CompletionList | undefined | null> {
         return new Promise<CompletionList>((resolve, reject) => {
-          const acceptCharset = 'abcdefghijklmnopqrstuvwxyz';
+          const alphaCharset = 'acdghijklmnpqstuvwxyz';
+          const punctuationCharset = '\'\"()<>[],.?;:';
+
           let preEdit = '';
           for (const singleChar of context.option.input) {
-            if (!acceptCharset.includes(singleChar)) {
+            if (!alphaCharset.includes(singleChar)) {
               preEdit += singleChar;
             } else {
               break;
             }
           }
 
-          rimeCLI.getContext(context.option.input)
+          let inputString = '';
+          let offset = document.offsetAt(position);
+          let inputRange: Range = { start: document.positionAt(offset), end: document.positionAt(offset), };
+          if (offset != 0) {
+            let singleChar = document.getText({
+              start: document.positionAt(offset - 1),
+              end: document.positionAt(offset),
+            });
+            while ((alphaCharset.includes(singleChar) || punctuationCharset.includes(singleChar)) && offset != 0) {
+              inputString = singleChar + inputString;
+              offset -= 1;
+              singleChar = document.getText({
+                start: document.positionAt(offset - 1),
+                end: document.positionAt(offset),
+              });
+            }
+            inputRange.start = document.positionAt(offset);
+          }
+
+          rimeCLI.getContext(inputString)
           .then((res) => {
+            // const sampleItem: CompletionItem = {
+            //   label: 'newText',
+            //   sortText: context.option.input,
+            //   filterText: context.option.input,
+            //   textEdit: {
+            //     range: inputRange,
+            //     newText: 'newText',
+            //   }
+            // };
+            // let { range } = sampleItem.textEdit;
+            // let newText: string = sampleItem.textEdit.newText;
+            // if (range && range.start.line == range.end.line) {
+            //   let { line, col, colnr } = context.option;
+            //   let character = col;
+            //   if (range.start.character > character) {
+            //     let before = line.slice(character - range.start.character)
+            //     newText = before + newText
+            //   } else {
+            //     let start = line.slice(range.start.character, character)
+            //     if (start.length && newText.startsWith(start)) {
+            //       newText = newText.slice(start.length)
+            //     }
+            //   }
+            //   character = colnr - 1;
+            //   if (range.end.character > character) {
+            //     let end = line.slice(character, range.end.character)
+            //     if (newText.endsWith(end)) {
+            //       newText = newText.slice(0, - end.length)
+            //     }
+            //   }
+            //   workspace.showMessage('newText: ' + newText + ', start: ' + inputRange.start.character + ', end: ' + inputRange.end.character);
+            // }
+            // resolve({
+            //   items: [sampleItem],
+            //   isIncomplete: false,
+            // });
+
             rimeCLI.getCompletionStatus()
             .then((isEnabled) => {
               if (isEnabled && res != null && 'menu' in res && res.menu != null && 'candidates' in res.menu && res.menu.candidates != null) {
@@ -54,7 +112,10 @@ export async function activate(context: ExtensionContext): Promise<void> {
                       label: preEdit + candidate.text,
                       sortText: context.option.input + candidate.label.toString().padStart(8, '0'),
                       filterText: context.option.input,
-                      insertText: preEdit + candidate.text,
+                      textEdit: {
+                        range: inputRange,
+                        newText: candidate.text,
+                      }
                     }
                   }),
                   isIncomplete: false,
@@ -78,9 +139,9 @@ export async function activate(context: ExtensionContext): Promise<void> {
             console.log(`Error getting Context: ${e}`);
             reject(e);
           });
-        })
+        });
       }
-    },   [], userConfig.priority, [])
+    }, [], userConfig.priority, [])
   );
 
   // Schema List
