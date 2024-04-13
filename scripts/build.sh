f3edf888df0f849a8b5b8e188435c62761721b88
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-04-13 23:08:05 +0800

    chore: add build script

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$(readlink -f "$0")")/.."
-
-gyp binding.gyp --depth=. --generator-output=build
-make -Cbuild
-mkdir -p build/Release
-ln -sf ../out/Default/rime_cli build/Release
