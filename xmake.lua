62928bead802cd22f9e7b89a30a27ba47caa3f0b
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-23 00:46:32 +0800

    :heavy_minus_sign: Remove node-api-headers because node-addon-api depends on it

--- xmake.lua
+++ xmake.lua
@@ -2,12 +2,12 @@
 ---@diagnostic disable: undefined-global
 add_rules("mode.debug", "mode.release")
 
-add_requires("rime", "node-addon-api", "node-api-headers")
+add_requires("rime", "node-addon-api")
 
 target("rime")
 do
     set_languages("cxx17")
     add_rules("nodejs.module")
-    add_packages("rime", "node-addon-api", "node-api-headers")
+    add_packages("rime", "node-addon-api")
     add_files("*.cc")
 end
